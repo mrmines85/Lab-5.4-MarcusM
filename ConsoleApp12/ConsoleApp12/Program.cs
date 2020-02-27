a076f220326d5f506b72601cf4822cbe32dd6b46
mrmines85 <61034810+mrmines85@users.noreply.github.com>
2020-02-27 17:29:12 -0600

    Add files via upload
    Lab 5.4 MarcusM

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        public struct Mario
+        {
+            public enum Size
+            {
+                small,
+                large
+            }
+            public int numOfLives;
+            public string currentLevel;
+            public Size sizeOfMario;
+            public bool canShootFireballs;
+            public bool isJumping;
+            public int numOfCoins;
+
+        }
+        static void Main(string[] args)
+        {
+           Mario mario = new Mario();
+            mario.numOfLives = 3;
+            mario.currentLevel = "World 1-1";
+            mario.sizeOfMario = Mario.Size.small;
+            mario.canShootFireballs = false;
+            mario.isJumping = false;
+
+            Console.WriteLine("Mario is running through the level and meets a Gooba. Set Mario to jumping so he can kill it");
+
+        }
+    }
+}
